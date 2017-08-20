db139e2f8964f47a1d1ecafb8ec86aa3cff132db
vadim <gabderahmanov99@gmail.com>
2017-08-20 11:03:59 +0400

    delete JComponentDragging class

--- src/com/vad/scheduler/Run.java
+++ src/com/vad/scheduler/Run.java
@@ -6,7 +6,6 @@ import java.awt.Color;
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
-import javax.swing.JLabel;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
@@ -19,10 +18,9 @@ public class Run {
 	static JButton addObj = new JButton("Add");
 	static JButton delete = new JButton("Delete");
 
-	static JLabel close = new JLabel("X");
-
 	static JPanel panelx = new JPanel();
 	static JPanel panelMenu = new JPanel();
+	static JPanel mainPanel = new JPanel();
 
 	static JMenuBar menuBar = new JMenuBar();
 	static JMenu menu = new JMenu("Window");
@@ -30,10 +28,11 @@ public class Run {
 
 	public static void main(String[] args) {
 
-		panelx.setLayout(new BoxLayout(panelx, BoxLayout.Y_AXIS));
+		panelx.setLayout(new BoxLayout(panelx, BoxLayout.X_AXIS));
 		panelMenu.setLayout(new BoxLayout(panelMenu, BoxLayout.X_AXIS));
 
-		ActionList.Action(close, panelx);
+		mainPanel.setLayout(new BorderLayout());
+
 		ActionList.ActionObj(mitem, panelx);
 
 		panelMenu.add(menuBar);
@@ -41,18 +40,17 @@ public class Run {
 		menuBar.add(menu);
 		menu.add(mitem);
 
-		panelx.setBounds(34, 34, 70, 100);
-
-		panelx.add(close);
 		panelx.add(addObj);
 		panelx.add(delete);
+
+		mainPanel.add("North", panelMenu);
+		mainPanel.add("South", panelx);
 
 		panelx.setBackground(Color.LIGHT_GRAY);
 
 		frame.setLayout(new BorderLayout());
 
-		frame.add(panelx);
-		frame.add("North", panelMenu);
+		frame.add("North", mainPanel);
 
 		graf.graph(frame, addObj, delete);
 
@@ -60,8 +58,6 @@ public class Run {
 		frame.setVisible(true);
 		frame.setLocation(400, 400);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-		JComponentDragging.belegeFlaechenMitListener(panelx);
 
 	}
 
